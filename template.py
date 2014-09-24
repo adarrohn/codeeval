4dba9c062ba7d8ee4c439bb71dcc5462eaf69cde
ga29civ <andrew.darrohn@tum.de>
2014-09-24 11:26:17 +0200

    init commit

---
+++ template.py
@@ -0,0 +1,15 @@
+'''
+Codeeval # ()
+'''
+
+import sys
+
+
+def function(line):
+    pass
+
+input_file = open(sys.argv[1], 'r')
+for line in input_file:
+    function(line)
+
+input_file.close()
---
+++ tests/template_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+class Test(unittest.TestCase):
+    """
+
+    """
+    def setUp(self):
+        pass
+
+    def test_case(self):
+        """
+
+        """
+        self.assertEquals(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
